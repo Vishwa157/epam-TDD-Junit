787d43bae346fa2808929b4b4d45d1bcda2d5bd7
Vishwanath Reddy <Vishwanath Reddy@192.168.0.8>
2020-07-23 13:00:51 +0530

    epaam task on TDD&Junit

---
+++ TDD_JUNIT/src/main/java/epam_Task/Testing.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package epam_Task;
+
+/**
+ * @author Vishwanath Reddy
+ *
+ */
+public class Testing {
+	public String BringIt(String s)
+	{
+		if(s=="")
+			return s;
+		else if(s.length()==1&&s.charAt(0)=='A')
+			return "";
+		else if(s.length()==1&&s.charAt(0)!='A')
+			return s;
+		else if(s.charAt(0)=='A'&&s.charAt(1)=='A')
+			return s.substring(2,s.length());
+		else if(s.charAt(0)=='A')
+			return s.substring(1,s.length());
+		else if(s.charAt(1)=='A')
+			return s.substring(0,1)+s.substring(2,s.length());
+		else 
+			return s;
+	}
+}
---
+++ TDD_JUNIT/src/test/java/Test.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import epam_Task.Testing;
+
+public class Test {
+
+	Testing test=new Testing();
+	
+	@org.junit.Test
+	public void test1() {
+		assertEquals("",test.BringIt(""));
+	}
+	
+	@org.junit.Test
+	public void test2() {
+		assertEquals("B",test.BringIt("B"));
+	}
+	
+	@org.junit.Test
+	public void test3()	{
+		assertEquals("B",test.BringIt("AAB"));	
+	}	
+	
+	@org.junit.Test
+	public void test4()	{
+		assertEquals("BCD",test.BringIt("ABCD"));	
+	}	
+	
+	@org.junit.Test
+	public void test5()	{
+		assertEquals("BCD",test.BringIt("BACD"));	
+	}	
+	
+	@org.junit.Test
+	public void test6()	{
+		assertEquals("BCDE",test.BringIt("BCDE"));	
+	}
+}
